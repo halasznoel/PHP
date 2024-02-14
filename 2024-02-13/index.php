417648b48b4466e42a452389455bfaa4cac05ee5
halasznoel <halasz.noel.2021e@ms.gdszeged.hu>
2024-02-14 22:44:07 +0100

    mentes

--- 2024-02-13/index.php
+++ 2024-02-13/index.php
@@ -1,75 +1,53 @@
 <!DOCTYPE html>
-<html lang="hu">
+<html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Három eurós bolt</title>
+    <title>Document</title>
 </head>
 <body>
-    <h1>Három eurós bolt</h1>
     <form action="index.php" method="get">
-        <p>Termék kiválasztása:</p>
         <select name="termek">
-            <?php
-            $termekek = array("Alma", "Körte", "Szőlő", "Banán", "Narancs");
-            foreach ($termekek as $termek) {
-                echo "<option value=\"$termek\">$termek</option>";
-            }
+            <?php 
+                $termekek = array("pacal","körte","ohio","hellnah","yes");
+                foreach($termekek as $termek){
+                    echo "<option value=\"$termek\">$termek<option>";
+                }
             ?>
         </select>
         <br><br>
-        <p>Termék ára (Ft):</p>
+        <label>Ár/termék</label>
         <input type="number" name="ar" value="1000">
         <br><br>
-        <p>Mennyiség:</p>
-        <input type="number" name="mennyiseg" value="1">
+        <label>Mennyiség</label>
+        <input type="number" name="mennyiseg">
         <br><br>
-        <p>Árengedmény (%):</p>
-        <input type="number" name="engedmeny" value="0">
-        <p>Akarunk árengedményt?</p>
-        <input type="checkbox" name="engedmeny_igen" value="igen">
+        <label>Árengedmény (%)</label>
+        <input type="number" name="arengedmeny">
         <br><br>
-        <input type="submit" value="Fizetés">
+        <label>Akarunk árengedményt</label>
+        <input type="checkbox" name="dontes" checked>
+        <br><br>
+        <input type="submit" value="Elküldés">
     </form>
-
-    <?php
+    <?php 
     $termek = "";
     $ar = 1000;
-    $mennyiseg = 1;
-    $engedmeny = 0;
-    $engedmeny_igen = false;
-    $brutto_ar = 0;
+    $mennyiseg = 0;
 
-    if (isset($_GET["termek"]) && isset($_GET['ar']) && isset($_GET["mennyiseg"]) && isset($_GET["engedmeny"]) && isset($_GET["engedmeny_igen"])) {
+    if(isset($_GET["termek"]) && isset($_GET["ar"]) && isset($_GET["mennyiseg"]) && isset($_GET["arengedmeny"])){
         $termek = $_GET["termek"];
-        $ar = (int)$_GET["ar"];
-        $mennyiseg = (int)$_GET["mennyiseg"];
-        $engedmeny = (int)$_GET["engedmeny"];
-        $engedmeny_igen = true;
-      
-        // Ellenőrzések
-        if ($mennyiseg <= 0) {
-          echo "<p>A mennyiség nem lehet 0!</p>";
-          exit;
-        }
-        if ($engedmeny < 0 || $engedmeny > 100) {
-          echo "<p>Az árengedmény 0 és 100% között kell, hogy legyen!</p>";
-          exit;
-        }
-      
-        $brutto_ar = $ar * $mennyiseg;
-        $engedmeny_szazalek = $engedmeny / 100;
-        $netto_ar = $brutto_ar - ($brutto_ar * $engedmeny_szazalek);
+        $ar = $_GET["ar"];
+        $mennyiseg = $_GET["mennyiseg"];
+        $arengedmeny = $_GET["arengedmeny"];
 
-        echo "<p>Termék: $termek</p>";
-        echo "<p>Mennyiség: $mennyiseg db</p>";
-        echo "<p>Darabár: $ar Ft</p>";
-        echo "<p>Ár: $brutto_ar Ft</p>";
-        echo "<p>Engedményes ár: $netto_ar Ft</p>";
-        echo "<p>Engedmény: $engedmeny %</p>";
-      
-      }
-      
+        $vegleges_ar = $ar * $mennyiseg;
+        $szazalek = $vegleges_ar - ($arengedmeny / 100 * $vegleges_ar);
+
+        echo "<p>Termék neve: $termek</p>";
+        echo "<p>Termék végleges ára: $vegleges_ar Ft</p>";
+        echo "<p>Termék ára akcióval: $szazalek Ft</p>";
+    }
     ?>
 </body>
 </html>
