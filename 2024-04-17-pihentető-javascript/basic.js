a4892f15e25c6188e1b67501a5363e1b7f7d91df
Halász Noel <halasz.noel.2021e@ms.gdszeged.hu>
2024-04-17 11:35:20 +0200

    save

--- 2024-04-17-pihentető-javascript/basic.js
+++ 2024-04-17-pihentető-javascript/basic.js
@@ -1,6 +1,9 @@
 //Amikor minden betöltődött az oldalon, akkor hajtsa végre a start függvényt
 window.onload = start;
 
+number = 0;
+
+
 function start(){
     //Ha rákattintok egy adott id-jű elemre, akkor melyik függvény hajtódjon végre
     document.getElementById('gomb1').onclick = gomb1click;
@@ -8,6 +11,10 @@ function start(){
     document.getElementById('piroshatter').onclick = piroshatterclick;
     document.getElementById('zoldhatter').onclick = zoldhatterclick;
     document.getElementById('changebackgroundbutton').onclick = hattervaltoztatas;
+    document.getElementById('divecske').onmouseover = divecske;
+    document.getElementById('divecske').onmouseout = divrollejotteger;
+    document.getElementById('budi').onmousemove = mozog;
+    window.onmousemove = mozog;
 }
 
 function gomb1click(){
@@ -29,7 +36,26 @@ function hattervaltoztatas(){
     document.body.bgColor=document.getElementById('color').value;
 }
 
+function divecske(){
+    document.getElementById('divecske').style.backgroundColor="green";
+}
+
+function divrollejotteger(){
+    document.getElementById('divecske').style.backgroundColor="red";
+}
+
+function mozog(){
+    number++;
+    if(number >= 100)
+        document.getElementById('divecske').innerHTML = "<p>Ne csináld már, unom!</p>";
+    else
+        document.getElementById('divecske').innerHTML = "<p>" + number + "</p>";
+}
+
 /*Események:
     onload: betöltődött
     onclick: rákattintottunk
+    onmouseover: rajta van az egér
+    onmouseout: lejött róla az egér
+    onmousemove: mozgatjuk rajta az egeret
 */
